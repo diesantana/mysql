d846961940a3f24aa3bf3064ec01c42a2998fca8
Diego Santana <3c.santana@gmail.com>
2024-03-04 10:42:26 -0300

    solução do exercício sql de fixação 02

---
+++ 05-Subqueries/exercicio02.sql
@@ -0,0 +1,9 @@
+-- COMEDY: CATEGORY,  CADEGORY_ID 5
+USE sakila;
+SELECT actor.first_name, actor.last_name
+FROM actor
+JOIN film_actor ON actor.actor_id = film_actor.actor_id
+JOIN film ON film_actor.film_id = film.film_id
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE category.name = 'Comedy';
